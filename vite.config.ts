0f00ed06ab5f979bed9a469c243d96f1117c9a11
User <user@example.com>
2026-09-09 14:47:20 +0800

    chore: add tauri cli; ignore rust artifacts in vite watch

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default <ViteUserConfig>{
   plugins: [svelte()],
-  server: { port: 5173 },
+  server: { port: 5173, watch: { ignored: ['**/src-tauri/**', '**/dist/**', '**/node_modules/**'] } },
   clearScreen: false
 }
